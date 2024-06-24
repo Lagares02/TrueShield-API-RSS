cf42492202e046ff038427d8a30a8e15b72e08a3
lagares_02 <harithodevoz@gmail.com>
2024-06-24 09:59:16 -0500

    estableciendo los limites de TrueLevel

--- services/news.py
+++ services/news.py
@@ -126,7 +126,7 @@ def contrasting_rss(db: Session, keywords = [], subjects = []):
                     "Summary": news.summary,
                     "BodyText": news.body,
                     "Authors": news.authors,
-                    "TrueLevel": 0.60 # Nivel de veracidad establecido (de 0.0 hasta 1.0)
+                    "TrueLevel": 0.60 # Nivel de veracidad establecido (de 0.00 hasta 1.00)
                 })
 
         # Sort matched_news by match_score (descending)
